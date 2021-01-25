f6d06662f2b33336debaf95889aeddb05f30aacb
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-25 15:15:01 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -15,11 +15,7 @@ def load_annotations(data_folder):
         if not rec["rsid"] or pandas.isna(rec["rsid"]):
             logging.warning("No rsid for variant '%s'", rec["Annotation ID"])
             continue
-        _id = re.match(".* \((.*?)\)",rec["rsid"]).groups()[0]
-        # we'll remove space in keys to make queries easier. Also, lowercase is preferred
-        # for a BioThings API. We'll an helper function from BioThings SDK
-        process_key = lambda k: k.replace(" ","_").lower()
-        rec = dict_convert(rec,keyfn=process_key)
+        _id = rec["rsid"]
         # remove NaN values, not indexable
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
