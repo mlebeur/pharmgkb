d36af892cd3925930370a8aa334bde527aa7a095
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-25 16:19:19 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,7 +11,7 @@ def load_annotations(data_folder):
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-        _id = rec["release"] + "_" + rec["chromosome"] + "_" + rec["position"] + "_" + rec["reference"] + "_" + rec["alternative"]        # remove NaN values, not indexable
+        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]        # remove NaN values, not indexable
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
         
