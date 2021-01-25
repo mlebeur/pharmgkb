20944a78e36b236089919c08670ac181ffc8b4f2
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-25 16:03:53 +0100

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,22 @@
+import os, pandas, csv, re
+import numpy as np
+from biothings.utils.dataload import dict_convert, dict_sweep
+
+from biothings import config
+logging = config.logger
+
+def load_annotations(data_folder):
+    infile = os.path.join(data_folder,"Dbsnp.1000.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+
+        _id = rec["release"] + " " + rec["chromosome"] + " " + rec["position"] + " " + rec["reference"] + " " + rec["alternative"]
+        # remove NaN values, not indexable
+        rec = dict_sweep(rec,vals=[np.nan])
+        results.setdefault(_id,[]).append(rec)
+        
+    for _id,docs in results.items():
+        doc = {"_id": _id, "annotations" : docs}
+        yield doc
