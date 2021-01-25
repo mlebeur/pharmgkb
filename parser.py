eb976a2dedfc2cb7f8bd2ff802d2f38e548c0d86
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-25 16:16:34 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,13 +6,12 @@ from biothings import config
 logging = config.logger
 
 def load_annotations(data_folder):
-    infile = os.path.join(data_folder,"GnomadGenomes.1.tsv")
+    infile = os.path.join(data_folder,"GnomadGenomes.1.1000.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-        _id = rec["position"]
-        # remove NaN values, not indexable
+        _id = rec["release"] + "_" + rec["chromosome"] + "_" + rec["position"] + "_" + rec["reference"] + "_" + rec["alternative"]        # remove NaN values, not indexable
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
         
