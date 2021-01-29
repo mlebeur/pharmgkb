725694c23d9c057e3d4015d3b57b9bb504636f8a
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-29 16:35:05 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,55 +11,46 @@ logging = config.logger
 process_key = lambda k: k.replace(" ","_").lower()
 
 
-def load_annotations(data_folder):
-    infile = os.path.join(data_folder,"var_drug_ann.tsv")
+def load_dbsnp(data_folder):
+    infile = os.path.abspath("/opt/biothings/Dbsnp.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-        if not rec["Gene"] or pandas.isna(rec["Gene"]):
-            logging.warning("No gene information for annotation ID '%s'", rec["Annotation ID"])
-            continue
-        _id = re.match(".* \((.*?)\)",rec["Gene"]).groups()[0]
-        rec = dict_convert(rec,keyfn=process_key)
-        # remove NaN values, not indexable
+        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"] + "_" + rec["rsid"]         # remove NaN values, not indexable
+        results.setdefault(_id,[]).append(rec)
+        
+    for _id,docs in results.items():
+        doc = {"_id": _id, "dbsnp" : docs}
+        yield doc
+
+
+def load_GnomadGenomes(data_folder):
+    infile = os.path.abspath("/opt/biothings/GnomadGenomes.1.1000.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]        # remove NaN values, not indexable
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
+        
     for _id,docs in results.items():
-        doc = {"_id": _id, "annotations" : docs}
+        doc = {"_id": _id, "gnomadgenomes" : docs}
         yield doc
 
 
-def load_druglabels(data_folder):
-    infile = os.path.join(data_folder,"drugLabels.byGene.tsv")
-    assert os.path.exists(infile)
-    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records') 
-    for rec in dat:
-        label_ids = rec.pop("Label IDs").split(";")
-        label_names = rec.pop("Label Names").split(";")
-        assert len(label_ids) == len(label_names)
-        labels = []
-        for i,_ in enumerate(label_ids):
-            labels.append({"id" : label_ids[i],
-                           "name" : label_names[i]})
-        _id = rec["Gene ID"]
-        rec = dict_convert(rec,keyfn=process_key)
-        doc = {"_id" : _id, "drug_labels" : labels}
-        yield doc
-
-
-def load_occurrences(data_folder):
-    infile = os.path.join(data_folder,"occurrences.tsv")
+def load_clinvar(data_folder):
+    infile = os.path.abspath("/opt/biothings/Clinvar.1000.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-        if rec["Object Type"] != "Gene":
-            continue
-        _id = rec["Object ID"]
-        rec = dict_convert(rec,keyfn=process_key)
+        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["start"]) + "_" + str(rec["end"]) + "_" + rec["reference"] + "_" + rec["alternative"]        # remove NaN values, not indexable
+        rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
+        
     for _id,docs in results.items():
-        doc = {"_id": _id, "occurrences" : docs}
+        doc = {"_id": _id, "clinvar" : docs}
         yield doc
 
