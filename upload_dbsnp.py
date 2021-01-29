d0ff8653a4d5c852b462cd7b9a5f25c40baf79c7
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-29 16:46:27 +0100

    Update upload_dbsnp.py

--- upload_dbsnp.py
+++ upload_dbsnp.py
@@ -11,14 +11,14 @@ from .parser import load_annotations
 class AnnotationsUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
 
     main_source = "pharmgkb"
-    name = "annotations"
+    name = "dbsnp"
     __metadata__ = {"src_meta": {}}
     idconverter = None
     storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
-        return load_annotations(data_folder)
+        return load_dbsnp(data_folder)
 
     @classmethod
     def get_mapping(klass):
