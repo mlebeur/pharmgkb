badd3a822eb4615429633d8b334a36c20997ec41
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-29 16:47:34 +0100

    Update and rename upload_druglabels.py to upload_GnomadGenomes.py

--- upload_GnomadGenomes.py
+++ upload_GnomadGenomes.py
@@ -11,14 +11,14 @@ from .parser import load_druglabels
 class DrugLabelsUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
 
     main_source = "pharmgkb"
-    name = "druglabels"
+    name = "GnomadGenomes"
     __metadata__ = {"src_meta": {}}
     idconverter = None
     storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
-        return load_druglabels(data_folder)
+        return load_GnomadGenomes(data_folder)
 
     @classmethod
     def get_mapping(klass):
