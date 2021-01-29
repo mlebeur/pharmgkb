17ce38d71d6f1f2db9d7373c7ded298890b83a07
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-01-29 16:48:11 +0100

    Update upload_occurrences.py

--- upload_occurrences.py
+++ upload_occurrences.py
@@ -11,14 +11,14 @@ from .parser import load_occurrences
 class OccurrencesUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
 
     main_source = "pharmgkb"
-    name = "occurrences"
+    name = "clinvar"
     __metadata__ = {"src_meta": {}}
     idconverter = None
     storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
-        return load_occurrences(data_folder)
+        return load_clinvar(data_folder)
 
     @classmethod
     def get_mapping(klass):
